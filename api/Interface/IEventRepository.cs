40627979f6596958b1fd7ab8126dfcf43ca3b25c
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2024-09-19 12:19:38 +0530

    add init structure

---
+++ api/Interface/IEventRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ebooking.Interface
+{
+    public interface IEventRepository
+    {
+        
+    }
+}
